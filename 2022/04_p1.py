9f7bbb86627015eb15560ca0fcb4424b5a8d0edc
Julien <6372605+fetzu@users.noreply.github.com>
2023-01-03 14:47:26 +0100

    add: day 4
    Another set of terrible code...

---
+++ 2022/04_p1.py
@@ -0,0 +1,24 @@
+# AoC 2022, Day 4: Camp Cleanup // Part 1
+f = open("04_data.txt","r")
+lines = f.readlines()
+
+count = 0
+i = 0
+buffer = []
+
+for line in lines:
+    pairs = line.split(',')
+    
+    for pair in pairs:
+        item = pair.split('-')
+        buffer.append(item)
+        i += 1
+        if i == 2 and int(buffer[0][0]) <= int(buffer[1][0]) and int(buffer[0][1]) >= int(buffer[1][1]):
+            count += 1
+        elif i == 2 and int(buffer[0][0]) >= int(buffer[1][0]) and int(buffer[0][1]) <= int(buffer[1][1]):
+            count += 1
+        if i == 2:
+            buffer = []
+            i = 0
+
+print(count)
