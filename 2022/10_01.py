ccd821a262481cc8e41bbffcf185621bd178a8e1
Julien <6372605+fetzu@users.noreply.github.com>
2023-01-08 00:51:51 +0100

    add: day 10, part 1

---
+++ 2022/10_01.py
@@ -0,0 +1,42 @@
+# AoC 2022, Day 1: Single Register // Part 1
+
+f = open("10_data.txt","r")
+instructions = f.readlines()
+
+X = 1
+cycle = 0
+queue = []
+results = []
+
+# Add up the results at given cycles
+def checkcycle(cycle, X, results):
+    if cycle == 20 or cycle == 60 or cycle == 100 or cycle == 140 or cycle == 180 or cycle == 220:
+        print(f"cycle {cycle}, X = {X} | adding {X * cycle} to result")
+        results.append((X * cycle))
+
+for instruction in instructions:
+    instruction = instruction.split()
+
+    # If the (FIFO) queue is not empty, cycle the first item
+    if len(queue) > 0:
+        print(f"cycles: {cycle} | queue")
+        cycle += 1
+        checkcycle(cycle, X, results)
+        X = X + int(queue[0])
+        queue.pop(0)
+
+    # Take care of the next instruction in the input data
+    if instruction[0] == "noop":
+        print(f"cycles: {cycle} | noop")
+        cycle += 1    
+        checkcycle(cycle, X, results)
+    elif instruction[0] == "addx":
+        print(f"cycles: {cycle} | addx")
+        cycle += 1
+        queue.append(instruction[1])
+        checkcycle(cycle, X, results)
+
+print(f"cycles: {cycle}")
+#print(X)
+print(results)
+print(sum(results))
